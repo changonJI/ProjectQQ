35c95ae3abb5b91dc9880d9c4569d029eaa767df
changon_Ji <jcg_dev@naver.com>
2025-03-31 01:50:40 +0900

    feat: 테스트

---
+++ Assets/Editor/MenuItemUtil.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class MenuItemUtil
+{
+    private const string ctrl = "%";
+    private const string alt = "&";
+    private const string shift = "#";
+
+#if UNITY_EDITOR
+    #region MenuItemName : Util
+    [MenuItem("Util/OpenGameScene " + alt + shift + "1", false, 0)]
+    private static void OpenGameScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/GameScene.unity");
+    }
+    #endregion
+
+#endif
+}
