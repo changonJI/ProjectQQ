50584989e37b2fbae43aaa225998831e691906fa
changon_Ji <jcg_dev@naver.com>
2025-04-03 18:51:04 +0900

    ✨ feat: namespace 추가

--- Assets/ProjectQQ/Scripts/Common/EnumTable.cs
+++ Assets/ProjectQQ/Scripts/Common/EnumTable.cs
@@ -1,16 +1,17 @@
-
-public enum UIDepth
+namespace QQ
 {
-    HUD = 0,
-    Fixed1,
-    Fixed2, 
-    Fixed3,
-    Popup,
-}
+    public enum UIDepth
+    {
+        HUD = 0,
+        Fixed1,
+        Fixed2,
+        Fixed3,
+        Popup,
+    }
 
-public enum Layer
-{ 
-    Default = 0,
-    UI = 5,
+    public enum Layer
+    {
+        Default = 0,
+        UI = 5,
+    }
 }
-
