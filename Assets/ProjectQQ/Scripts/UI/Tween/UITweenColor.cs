9046fb98cd6e9a0d0fa4db2cf38eea9e442e0e9a
changon_Ji <jcg_dev@naver.com>
2025-04-17 18:54:00 +0900

    ✨ feat: UITweenColor 기능 추가

---
+++ Assets/ProjectQQ/Scripts/UI/Tween/UITweenColor.cs
@@ -0,0 +1,25 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITweenColor : MonoBehaviour
+{
+    private Image image;
+    [SerializeField] private Color EndColor;
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private Ease ease = Ease.Linear;
+    [SerializeField] private bool isLoop = false;
+    [SerializeField] private LoopType loopType = LoopType.Restart;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+
+        if (image == null)
+            return;
+
+        image.DOColor(EndColor, duration)
+            .SetEase(ease)
+            .SetLoops(isLoop ? -1 : 0, loopType);
+    }
+}
