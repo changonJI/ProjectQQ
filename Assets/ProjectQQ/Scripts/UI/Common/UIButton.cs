e46b9c89522813284df8861f0cba1129ecfc5a77
changon_Ji <jcg_dev@naver.com>
2025-04-23 21:48:30 +0900

    ✨ feat: UIButton, UIImage 수정

--- Assets/ProjectQQ/Scripts/UI/Common/UIButton.cs
+++ Assets/ProjectQQ/Scripts/UI/Common/UIButton.cs
@@ -1,22 +1,19 @@
+using UnityEditor;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Button))]
-public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+[RequireComponent(typeof(Image))]
+[AddComponentMenu("UI/UIButton")]
+public class UIButton : Button
 {
-    private Button button;
-    
-    [SerializeField] private UnityEvent onLongClickEvent;
+    [SerializeField] private UnityEvent onLongClick;
+
     private bool isPressed = false;
     private float pressTime = 0f;
     private float longClickTime = 1.0f;
-
-    private void Awake()
-    {
-        button = GetComponent<Button>();
-    }
 
     private void Update()
     {
@@ -25,11 +22,11 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
             pressTime += Time.deltaTime;
         }
     }
-
+    
     /// <summary>
     /// 버튼 범위 안에서 마우스를 누르는 순간의 이벤트
     /// </summary>
-    public void OnPointerDown(PointerEventData eventData)
+    public override void OnPointerDown(PointerEventData eventData)
     {
         SetTime(0f);
         SetPress(true);
@@ -38,7 +35,7 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     /// <summary>
     /// 버튼 범위 상관없이 마우스를 누르고 땠을 때 발생하는 이벤트
     /// </summary>
-    public void OnPointerUp(PointerEventData eventData)
+    public override void OnPointerUp(PointerEventData eventData)
     {
         SetPress(false);
     }
@@ -46,15 +43,15 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     /// <summary>
     /// 버튼 범위 안에서 마우스를 누르고 땟을 때 발생하는 이벤트
     /// </summary>
-    public void OnPointerClick(PointerEventData eventData)
+    public override void OnPointerClick(PointerEventData eventData)
     {
         if (pressTime >= longClickTime)
         {
-            onLongClickEvent?.Invoke();
+            onLongClick?.Invoke();
         }
         else
         {
-            button.onClick.Invoke();
+            onClick?.Invoke();
         }
 
         SetPress(false);
@@ -63,10 +60,45 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     private void SetPress(bool press)
     {
         isPressed = press;
-        
+
     }
     private void SetTime(float time)
     {
         pressTime = time;
+    }
+
+    /// <summary>
+    /// <see cref="ButtonEditor"/>
+    /// </summary>
+    [CustomEditor(typeof(UIButton), true)]
+    private class UIButtonEditor : SelectableEditor
+    {
+        private UIButton owner;
+        SerializedProperty m_OnClickProperty;
+        SerializedProperty m_OnClickLongProperty;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            m_OnClickProperty = serializedObject.FindProperty("m_OnClick");
+
+            owner = (UIButton)target;
+            m_OnClickLongProperty = serializedObject.FindProperty(nameof(owner.onLongClick));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.Space();
+
+            serializedObject.Update();
+            
+            EditorGUILayout.PropertyField(m_OnClickProperty);
+
+            // 추가
+            EditorGUILayout.PropertyField(m_OnClickLongProperty);
+            
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
