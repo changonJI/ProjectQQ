40b13dcefe2b062c5cf34a01323e2ce3575594e8
changon_Ji <jcg_dev@naver.com>
2025-05-20 01:17:00 +0900

    ✨ feat: 확장형메소드 클래스 추가

---
+++ Assets/ProjectQQ/Scripts/Common/ExtensionMethods.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace QQ
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsValidArrange(this object[] objects, int index)
+        {
+            if (objects == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("objects is null");
+#endif
+                return false;
+            }
+
+            if(objects.Length > index)
+            {
+                return true;
+            }
+            else
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Over Arrange");
+#endif
+                return false;
+            }
+                
+        }
+    }
+
+}
