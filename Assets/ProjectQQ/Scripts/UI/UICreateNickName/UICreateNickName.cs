7850cc2b422fd22b612d9e589d913cce3e4ea40a
changon_Ji <jcg_dev@naver.com>
2025-05-20 02:13:07 +0900

    ♻️ refactor: 확정팝업, 닉네임 설정 팝업 코드 수정

---
+++ Assets/ProjectQQ/Scripts/UI/UICreateNickName/UICreateNickName.cs
@@ -0,0 +1,81 @@
+using TMPro;
+using UnityEngine;
+
+namespace QQ
+{
+    /// <summary>
+    /// 텍스트 입력 팝업
+    /// </summary>
+    public class UICreateNickName : UI<UICreateNickName>
+    {
+        public override UIType uiType => UIType.Destroy;
+
+        public override UIDepth uiDepth => UIDepth.Fixed2;
+
+        private const int limitTxt = 8;
+
+        [SerializeField] private TextMeshProUGUI txtTitle;
+        [SerializeField] private TextMeshProUGUI txtDefault;
+        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private UIButton btnEnter;
+
+        protected override void OnInit()
+        {
+            txtTitle.text = "타이틀";
+            inputField.text = string.Empty;
+            txtDefault.text = "입력칸";
+        }
+
+        protected override void OnStart()
+        {
+            inputField.onValueChanged.AddListener(OnChanged);
+            inputField.onEndEdit.AddListener(OnEndEdit);
+            inputField.onSelect.AddListener(OnSelect);
+        }
+
+        protected override void OnFocus()
+        {
+        }
+
+        protected override void OnLostFocus()
+        {
+        }
+
+        public void OnClickConfirm()
+        {
+            if (inputField.text.Length > limitTxt)
+            {
+                Debug.LogError("글자 초과. 팝업 띄울것. 다시 입력하세요");
+            }
+            else
+            {
+                //PlayerPrefs.SetString("NickName", inputField.text);
+                Close();
+            }
+        }
+
+        public void OnChanged(string input)
+        {
+            if (inputField.text.Length > limitTxt)
+            {
+                inputField.text = inputField.text.Substring(0, limitTxt);
+            }
+        }
+
+        public void OnEndEdit(string input)
+        {
+            OnClickConfirm();
+        }
+
+        public void OnSelect(string input)
+        {
+            inputField.text = string.Empty;
+        }
+
+        public void OnDeSelect(string input)
+        {
+            inputField.text = string.Empty;
+        }
+    }
+
+}
