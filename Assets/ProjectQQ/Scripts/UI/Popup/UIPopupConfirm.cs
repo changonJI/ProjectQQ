7705ac0529868844fdd78589a70d753155a26407
changon_Ji <jcg_dev@naver.com>
2025-05-20 01:14:57 +0900

    ✨ feat: UIPopupConfirm_확정 팝업 프리팹 및 코드 수정

--- Assets/ProjectQQ/Scripts/UI/Popup/UIPopupConfirm.cs
+++ Assets/ProjectQQ/Scripts/UI/Popup/UIPopupConfirm.cs
@@ -1,7 +1,5 @@
-using System;
-using Cysharp.Threading.Tasks;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 
 namespace QQ
@@ -12,56 +10,40 @@ namespace QQ
     public sealed class UIPopupConfirm : UIPopup<UIPopupConfirm>
     {
         [SerializeField] private TextMeshProUGUI messageTextTMP;
-        [SerializeField] private UIButton okButton;
-        [SerializeField] private UIButton cancelButton;
-
+        [SerializeField] private UIButtonAndText btnOk;
+        [SerializeField] private UIButtonAndText btnCancel;
+        
         protected override void OnInit()
         {
-            base.OnInit();
         }
 
         protected override void OnStart()
         {
+            
         }
 
         protected override void OnFocus()
         {
+            SetTitle(GetParamToString(0));
+
+            btnOk.SetText(GetParamToString(1));
+            btnCancel.SetText(GetParamToString(2));
+
+            btnOk.OnClickAdd(OnClickOk);
+            btnCancel.OnClickAdd(OnClickClose);
         }
 
         protected override void OnLostFocus()
         {
+            btnOk.OnClickRemove(OnClickOk);
+            btnCancel.OnClickRemove(OnClickClose);
         }
 
         /// <summary>
-        /// 확인 팝업 버튼 동작 세팅
+        /// 팝업 제목 세팅
         /// </summary>
-        /// <param name="onOk">확인 버튼 OnClick 동작</param>
-        /// <param name="onCancel">취소 버튼 OnClick 동작</param>
-        /// <param name="tcs">확인 취소 버튼 클릭 결과 받아갈 변수</param>
-        public void SetBtnAction(Action onOk = null, Action onCancel = null, UniTaskCompletionSource<bool> tcs = null)
-        {
-            okButton.onClick.AddListener(() =>
-            {
-                onOk?.Invoke();
-                tcs?.TrySetResult(true);
-
-                Close();
-            });
-
-            cancelButton.onClick.AddListener(() =>
-            {
-                onCancel?.Invoke();
-                tcs?.TrySetResult(false);
-
-                Close();
-            });
-        }
-
-        /// <summary>
-        /// 텍스트 세팅
-        /// </summary>
-        /// <param name="messageText">팝업 안내 문구</param>
-        public void SetText(string messageText)
+        /// <param name="messageText"></param>
+        private void SetTitle(string messageText)
         {
             messageTextTMP.text = messageText;
         }
