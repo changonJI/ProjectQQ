0219a4f5e6691161e67fcb26d6c47e1e8de86e68
Honyonyo <115960532+Honyonyo@users.noreply.github.com>
2025-06-11 22:01:17 +0900

    ✨ feat: 움직임 관련 Movement 코드 추가

---
+++ Assets/ProjectQQ/Scripts/Game/Movement/MovementBase.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace QQ
+{
+    // TODO
+    // * 이동 각도 8방향으로 통제해야 함
+    // ~ 고려해야할 것
+    // 일시적인 이동속도 증가, 감소 효과(중첩가능) 만들어야할듯
+    // 
+
+
+    /// <summary>
+    /// 기본 이동
+    /// </summary>
+    [DisallowMultipleComponent]
+    public abstract class MovementBase : MonoBehaviour
+    {
+        private Rigidbody2D rigidBody;
+        [SerializeField] protected float speedBase;
+        [SerializeField] protected Vector2 vec2Direction;
+
+        public float Velocity => speedBase;
+
+        #region Unity Method
+        protected virtual void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+            if (null == rigidBody)
+            {
+                LogHelper.LogError($"MovementBase {gameObject.name} 리지드바디2D가 없음");
+            }
+
+            OnInit();
+        }
+
+        protected virtual void Start()
+        {
+            InputManager.Instance.OnMoveInput += HandleMoveInput;
+            OnStart();
+        }
+
+        protected virtual void Update()
+        {
+            InputManager.Instance.OnMoveInput -= HandleMoveInput;
+            OnUpdate();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            Move(vec2Direction, Velocity);
+
+            OnFixedUpdate();
+        }
+
+        void HandleMoveInput(Vector2 dir)
+        {
+            vec2Direction = dir;
+
+            OnMoveHandled();
+        }
+
+        #endregion
+
+        protected virtual void Move(Vector2 dir, float velocity)
+        {
+            Vector2 vec2DeltaMovement = dir * velocity * Time.fixedDeltaTime;
+            rigidBody.MovePosition(rigidBody.position + vec2DeltaMovement);
+        }
+
+        protected abstract void OnInit();
+        protected abstract void OnStart();
+        protected abstract void OnUpdate();
+        protected abstract void OnFixedUpdate();
+        protected abstract void OnMoveHandled();
+    }
+}
---
+++ Assets/ProjectQQ/Scripts/Game/Movement/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace QQ
+{
+    public class PlayerMovement : MovementBase
+    {
+        protected override void OnInit() { }
+        protected override void OnStart() { }
+        protected override void OnUpdate() { }
+        protected override void OnFixedUpdate() { }
+        protected override void OnMoveHandled() { }
+    }
+}
