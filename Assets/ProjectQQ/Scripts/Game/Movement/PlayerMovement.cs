8e67e70fc276f92ceb2428d1f29575c4b1b334f0
Honyonyo <115960532+Honyonyo@users.noreply.github.com>
2025-06-15 04:41:19 +0900

    ✨ feat: 플레이어 구르기 동작 로그 추가

--- Assets/ProjectQQ/Scripts/Game/Movement/PlayerMovement.cs
+++ Assets/ProjectQQ/Scripts/Game/Movement/PlayerMovement.cs
@@ -4,6 +4,7 @@ namespace QQ
 {
     public class PlayerMovement : MovementBase
     {
+        private bool isRollStart = false;
         protected override void OnInit() { }
 
         protected override void OnStart()
@@ -17,11 +18,47 @@ namespace QQ
         }
 
         protected override void OnUpdate() { }
-        protected override void OnFixedUpdate() { }
+        protected override void OnFixedUpdate()
+        {
+            if (true == isRollStart)
+            {
+                Roll(lastMoveDirection);
+                isRollStart = false;
+            }
+        }
 
         private void HandleMoveInput(Vector2 dir)
         {
             moveDirection = dir;
+        }
+        private void HandleRollInput()
+        {
+            isRollStart = true;
+        }
+
+        private void Roll(Vector2 dir)
+        {
+            // float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            string strDir = "";
+            if (0 < dir.x)
+            {
+                strDir = "오른쪽 ";
+            }
+            else if (0 > dir.x)
+            {
+                strDir = "왼쪽 ";
+            }
+
+            if (0 < dir.y)
+            {
+                strDir += "위";
+            }
+            else if (0 > dir.y)
+            {
+                strDir += "아래";
+            }
+
+            LogHelper.Log($"데구룽~ 방향 {strDir}");
         }
     }
 }
