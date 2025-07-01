7015aabe9fe04889a648f37dbbf363e86712e4b9
changon_Ji <jcg_dev@naver.com>
2025-07-02 02:27:44 +0900

    ✨ feat: Spine Anim의 Duration 값 확인 메소드 추가

--- Assets/ProjectQQ/Scripts/Common/SpineGameObject.cs
+++ Assets/ProjectQQ/Scripts/Common/SpineGameObject.cs
@@ -322,5 +322,15 @@ namespace QQ
             }
         }
 
+        public float GetAnimDuration(AnimState state)
+        {
+            string animName = GetAnimName(state);
+
+            var anim = animBody.skeleton.Data.FindAnimation(animName);
+
+            float duration = anim.Duration;
+
+            return duration;
+        }
     }
 }
