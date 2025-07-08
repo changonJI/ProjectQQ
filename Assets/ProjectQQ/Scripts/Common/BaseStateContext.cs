9b395ad571a07a6700717b7b0e53d7f5714858b3
Honyonyo <115960532+Honyonyo@users.noreply.github.com>
2025-07-08 19:14:08 +0900

    ✨ feat: FSM ChangeState() 상태 전이 동작시에 전이 허용된 상태인지 체크하는 과정 추가
    1. BaseStateContex에 각 상태별 전이 가능한 상태 목록 정의 추가 (AllowedTransrations)
    2. ChangeState() 동작시 전이 허용된 상태로 전이 시도하는지 검증하는 코드 추가

--- Assets/ProjectQQ/Scripts/Common/BaseStateContext.cs
+++ Assets/ProjectQQ/Scripts/Common/BaseStateContext.cs
@@ -1,14 +1,28 @@
 using QQ.FSM;
+using System.Collections.Generic;
 
 namespace QQ
 {
     public abstract class BaseStateContext
     {
+        // 각 상태(State)에서 전이(Transition)할 수 있는 상태 목록 정의
+        public static readonly Dictionary<FSMState, HashSet<FSMState>> AllowedTransrations = new()
+        {
+            { FSMState.Idle,        new HashSet<FSMState> { FSMState.Move, FSMState.Roll, FSMState.Knockback, FSMState.Die } },
+            { FSMState.Move,        new HashSet<FSMState> { FSMState.Idle, FSMState.Roll, FSMState.Knockback, FSMState.Die } },
+            { FSMState.Roll,        new HashSet<FSMState> { FSMState.Idle, FSMState.Move, FSMState.Die} },
+            { FSMState.Knockback,   new HashSet<FSMState> { FSMState.Idle, FSMState.Move, FSMState.Roll, FSMState.Die } },
+            { FSMState.Die,         new HashSet<FSMState> { FSMState.Idle } }
+        };
+
         private IState currentState;
 
         public void ChangeState(IState newState)
         {
             if (newState == currentState)
+                return;
+
+            if (false == AllowedTransrations[currentState.GetFSMType()].Contains(newState.GetFSMType()))
                 return;
 
             currentState?.Exit();
